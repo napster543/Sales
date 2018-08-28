f6d83c81f8ebb8f0695d93ede246a1cb9a318555
napster543 <napster543@gmail.com>
2018-08-28 13:42:56 -0500

    ProductsPage

---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Sales.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Sales.ViewModels
+{
+    public class ProductsViewModel : BaseViewModel
+    {
+        private ObservableCollection<Product> products;
+        public ObservableCollection<Product> Products
+        {
+            get { return this.products; }
+            set { SetValue(ref this.products, value); }
+        }
+        //https://www.youtube.com/watch?v=VQSj7fKbngg&index=7&list=PLuEZQoW9bRnRnzwx4z1kzoY2Pt2nve6L_
+        // Video minuto 26:28
+    }
+}
